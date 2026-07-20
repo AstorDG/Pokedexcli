6cec5ad1245c513960bc2ef86a22e9aaba8bb241
AstorDG <astormg999@gmail.com>
2026-07-19 21:31:27 -0400

    repeater REPL

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
-func main() {
-	fmt.Printf("Hello, World!")
-}
-
+// Lowercase and split a string on white space
 func cleanInput(input string) []string {
 	lower_input := strings.ToLower(input)
 	return strings.Fields(lower_input)
+}
+
+func main() {
+	user_input_scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex > ")
+		user_input_scanner.Scan()
+		user_input := user_input_scanner.Text()
+		split_user_input := cleanInput(user_input)
+		fmt.Printf("Your command was: %v\n", split_user_input[0])
+	}
 }
